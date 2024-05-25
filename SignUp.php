5f6b8f8b912f5241442e8159bca66053bbaa69e9
Gabriel Dominic Dicar <126478685+Badaboopie@users.noreply.github.com>
2024-05-25 21:36:03 +0800

    Add files via upload

---
+++ SignUp.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="SigninCSS.css">
+
+    <title>Sign Up</title>
+</head>
+<body>
+    <div class="navbar-header">
+        <ul>
+            <img src="ShareBestie_Logo.png" alt="LOGO" style="margin-left:5%; padding:0%; height:50px; width:50px; object-fit:cover;">
+            <a href="" id="brandname">ShareBestie</a>
+            <li><a href="">Careers</a></li>
+            <li><a href="">Courses</a></li>
+            <li><a href="">Tutors</a></li>
+        </ul>
+    </div>
+
+    <div class="signupform">
+    <h1 id="tableHeader">Create your account</h1>
+        <form action="">
+            <table>
+                <tr>
+                    <p id="gen-info">General Information</p>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" name="username" placeholder="Username">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" name="fullname" placeholder="Full Name">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="password" name="password" placeholder="Password">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="email" name="email" placeholder="Email">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button class="btn btn-primary" id="signup" type="submit">Sign Up</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </div>
+    
+</body>
+</html>
