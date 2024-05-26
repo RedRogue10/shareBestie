7a700f3908b2e4546c7597d22163e1ed29391226
Piage <piageaguirre@gmail.com>
2024-05-26 15:22:47 +0800

    Added tables to database

--- tables.php
+++ tables.php
@@ -1,13 +1,12 @@
 <?php
     include 'DBconnect.php';
 
-    // $sql = "CREATE TABLE coursePage(
-    //     CourseID VARCHAR(10),
-    //     Description TEXT,
-    //     Prereq VARCHAR(10),
-    //     PRIMARY KEY (Course ID),
-    //     FOREIGN KEY (Prereq) REFERENCES course(CourseID)
-    // )";
+    $sql = "CREATE TABLE course(
+        CourseID VARCHAR(10),
+        Description TEXT,
+        PRIMARY KEY (CourseID)
+    )";
+    $conn->query($sql);
 
     $sql = "CREATE TABLE user(
         UserID INT AUTO_INCREMENT,
@@ -15,45 +14,86 @@
         FName VARCHAR(40),
         LName VARCHAR(40),
         Email VARCHAR(40) Unique,
+        isTutor BOOLEAN DEFAULT false,
         Password VARCHAR(40),
         PRIMARY KEY (UserID)
     )";
-    $sql = "CREATE TABLE tutor(
-        TutorID INT AUTO_INCREMENT,
-        UserID INT AUTO_INCREMENT,
-    )";
-
-    $sql = "CREATE TABLE teaches(
-        TutorID INT,
-        
-    )";
-
     $conn->query($sql);
-    $conn()->close();
 
-    // $sql = "CREATE TABLE postUnder(
-    //     PostID INT AUTO_INCREMENT,
-    //     Title VARCHAR,
-    //     Content TEXT,
-    //     PostDate DATETIME,
-    //     CourseID VARCHAR(10),
-    //     UserID INT,
-    //     FOREIGN KEY (CourseID) REFERENCES coursePage(CourseID),
-    //     FOREIGN KEY (UserID) REFERENCES user(UserID),
-    //     PRIMARY KEY (PostID)
-    // )";
+    $sql = "CREATE TABLE post(
+        PostID INT AUTO_INCREMENT,
+        Title TEXT,
+        PostDate DATETIME,
+        Content TEXT,
+        PRIMARY KEY (PostID)
+    )";
+    $conn->query($sql);   
 
-    // $sql = "CREATE TABLE comments(
-    //     CommentID INT AUTO_INCREMENT,
-    //     UserID INT,
-    //     PostID INT,
-    //     Content TEXT,
-    //     CommentDate DATETIME,
-    // )";
+    $sql = "CREATE TABLE comment(
+        CommentID INT AUTO_INCREMENT,
+        Content TEXT,
+        PostDate DATETIME,
+        PRIMARY KEY (CommentID)
+    )";
+    $conn->query($sql);
 
-    // $sql = "CREATE TABLE replies(
-    //     CommentID INT,
-    //     ReplyID INT,
-    //     FOREIGN KEY (CommentID)  REFERENCES
-    // )"
+    $sql = "CREATE TABLE prereq(
+        CourseID VARCHAR(10),
+        PreReqID VARCHAR(10),
+        PRIMARY KEY (CourseID, PreReqID),
+        FOREIGN KEY (CourseID) REFERENCES course(CourseID),
+        FOREIGN KEY (PreReqID) REFERENCES course(CourseID)
+    )";
+    $conn->query($sql); 
 
+
+
+    $sql = "CREATE TABLE tutors(
+        TutorID INT,
+        UserID INT,
+        FOREIGN KEY (TutorID) REFERENCES user(UserID),
+        FOREIGN KEY (UserID) REFERENCES user(UserID),
+        PRIMARY KEY (TutorID, UserID)   
+    )";
+    $conn->query($sql);
+
+
+
+    $sql = "CREATE TABLE postUnder(
+        PostID INT,
+        CourseID VARCHAR(10),
+        FOREIGN KEY (PostID) REFERENCES post(PostID),
+        FOREIGN KEY (CourseID) REFERENCES course(CourseID),
+        PRIMARY KEY (PostID, CourseID)
+    )";
+    $conn->query($sql);
+
+    $sql = "CREATE TABLE posts(
+        PostID INT,
+        UserID INT,
+        FOREIGN KEY (PostID) REFERENCES post(PostID),
+        FOREIGN KEY (UserID) REFERENCES user(UserID),
+        PRIMARY KEY (PostID, UserID)
+    )";
+    $conn->query($sql);
+
+
+    
+    $sql = "CREATE TABLE replies(
+        CommentID INT,
+        ReplyID INT,
+        FOREIGN KEY (CommentID) REFERENCES comment(CommentID),
+        FOREIGN KEY (ReplyID) REFERENCES comment(CommentID),
+        PRIMARY KEY (CommentID, ReplyID)
+    )";
+    $conn->query($sql);
+
+    $sql = "CREATE TABLE comments(
+        CommentID INT,
+        UserID INT,
+        FOREIGN KEY (CommentID) REFERENCES comment(CommentID),
+        FOREIGN KEY (UserID) REFERENCES user(UserID),
+        PRIMARY KEY (CommentID, UserID)
+    )";
+    $conn->query($sql);
+    $conn->close();
