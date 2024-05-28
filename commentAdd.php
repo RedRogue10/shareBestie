2be9bcd3cb8b16789fc54607de76bb891aadd063
Piage <piageaguirre@gmail.com>
2024-05-28 10:56:14 +0800

    Fixed Comments

--- commentAdd.php
+++ commentAdd.php
@@ -5,14 +5,14 @@
     $content =  $_POST['content'];
     $postID = $_POST['PostID'];
     $date  = date ('Y-m-d H:i:s', time());
-    
-    $sql = 'INSERT INTO comment (Content,PostDate) VALUES("$content","$date")';
+
+    $sql = "INSERT INTO comment (Content,PostDate) VALUES('$content','$date')";
     $conn->query($sql);
     $commentID =  mysqli_insert_id($conn);
-
-    $sql ='INSERT INTO comments (CommentID, UserID) VALUES("$commentID","$userID")';
+    echo $commentID;
+    $sql ="INSERT INTO comments (CommentID, UserID) VALUES('$commentID','$userID')";
     $conn->query($sql);
-    $sql = 'INSERT INTO commentunder (CommentID, PostID) VALUES("$commentID","$postID")';
+    $sql = "INSERT INTO commentunder (CommentID, PostID) VALUES('$commentID','$postID')";
     $conn->query($sql);
 
     header("Location: postPage.php?postID=$postID");
