667985bcb9dfe6c7f0f0165379ae146be5a92e02
cdvincoy <99915734+cdvincoy@users.noreply.github.com>
2024-05-30 11:42:52 +0800

    Final

--- about.php
+++ about.php
@@ -1,10 +1,10 @@
-<!--Source Code for about.php-->
 <?php 
     session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--Source Code for about.php-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - shareBestie</title>
