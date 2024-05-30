bb501c5803cc1c920428e8527cebbd5b22cdb970
cdvincoy <99915734+cdvincoy@users.noreply.github.com>
2024-05-30 12:34:40 +0800

    Final

--- userAccount.php
+++ userAccount.php
@@ -12,6 +12,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--Source Code for userAccount.php-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - shareBestie</title>
@@ -23,8 +24,7 @@
 
 </head>
 <body>
-
-    <!-- Header Section -->
+<!--Header Section-->
     <header>
         <div class="logo">
             <img src="ShareBestie_Logo.png" alt="Your Website Logo">
@@ -35,35 +35,26 @@
                 <li><a href="home.php">Home</a></li>
                 <li><a href="courseSearch.php">Courses</a></li>
                 <li><a href="tutors.php">Tutors</a></li>
-                <li><a href="/about">About Us</a></li>
-               <!--  <li><a href="/login">Login</a></li>
-                <li><a href="signup">Sign up</a></li> -->
+                <li><a href="about.php ">About Us</a></li>
             </ul>
         </nav>
+        <!--Log In and Sign Up for Logged In and Nonlogged In Users-->
         <?php 
             if(isset($_SESSION['userID'])){
-                echo '<div class="user-auth">
-                        <!--Login Form -->
-                        <a href=""><button class="btn btn-primary" id="account" type="submit" >Account</button></a>
-      
-                        <!--Logout -->
+                echo '<div class="user-auth"
+                        <a href="userAccount.php"><button class="btn btn-primary" id="account" type="submit" >Account</button></a>
                         <a href="userLogout.php"><button class="btn btn-primary" id="logout" type="submit">Log Out</button></a> 
                     </div>';
             }else{
                 echo '<div class="user-auth">
-                        <!--Login Form -->
                         <a href="userLogin.php"><button class="btn btn-primary" id="login" type="submit" >Log in</button></a>
-      
-                        <!--Signup Form -->
                         <a href="userSignup.php"><button class="btn btn-primary" id="signup" type="submit" >Sign Up</button></a>
               </div>';
             }
-        
-        
         ?>
     </header>
-
-   <!-- Account Display -->
+    
+   <!--Account Display-->
     <section id="section1" class="section section2">
         <div class="welcome-content">
             <h1>Welcome to <br><strong>shareBestie!</strong></h1>
@@ -77,13 +68,13 @@
         </div>
     </section>
 
-    <!-- Footer Section -->
+    <!--Footer Sectio -->
     <footer>
-        <!-- Contact Information -->
+        <!--Contact Informatio -->
         <div class="contact-info">
-            <p>Contact Us: insert email here | Follow Us: <a href="#">Social Media</a></p>
+            <p>Contact Us: www.DVA@shareBestie.com | Follow Us: <a href="#">Facebook</a></p>
         </div>
-        <!-- Footer Links -->
+        <!--Footer Links-->
         <div class="footer-links">
             <ul>
                 <li><a href="/terms">Terms of Service</a></li>
