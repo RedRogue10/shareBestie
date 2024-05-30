62a134c9b96b31f47c333c82a1c14db37e91d8f9
cdvincoy <99915734+cdvincoy@users.noreply.github.com>
2024-05-30 11:52:47 +0800

    Final

--- coursePage.php
+++ coursePage.php
@@ -1,22 +1,21 @@
-<!DOCTYPE html>
 <?php 
     session_start();
     include 'dbConnect.php';
     $courseID = $_GET['id'];
 
-    // Query course information
+    //Query course information
     $sql = "SELECT * FROM course WHERE CourseId = '$courseID'";
     $courseRow = $conn->query($sql)->fetch_assoc();
     
-    // Query Posts Under Course
+    //Query Posts Under Course
     $sql = "SELECT * FROM postUnder NATURAL JOIN post WHERE CourseID = '$courseID'";
     $postsUnder = $conn->query($sql);
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--Source Code coursePage.php-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - shareBestie</title>
@@ -24,12 +23,10 @@
     <link rel="stylesheet" href="styles.css"> 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="ShareBestie_Logo.png">
-
-
 </head>
 <body>
 
-    <!-- Header Section -->
+    <!--Header Section-->
     <header>
         <div class="logo">
             <img src="ShareBestie_Logo.png" alt="Your Website Logo">
@@ -40,31 +37,26 @@
                 <li><a href="home.php">Home</a></li>
                 <li><a href="courseSearch.php">Courses</a></li>
                 <li><a href="tutors.php">Tutors</a></li>
-                <li><a href="about.html">About Us</a></li>
+                <li><a href="about.php">About Us</a></li>
             </ul>
         </nav>
+        <!--Log In and Sign Up for Logged In and Nonlogged In Users-->
         <?php 
             if(isset($_SESSION['userID'])){
                 echo '<div class="user-auth">
-                        <!--Login Form -->
                         <a href="userAccount.php"><button class="btn btn-primary" id="account" type="submit" >Account</button></a>
-      
-                        <!--Signup Form -->
                         <a href="userLogout.php"><button class="btn btn-primary" id="logout" type="submit">Log Out</button></a> 
                     </div>';
             }else{
                 echo '<div class="user-auth">
-                        <!--Login Form -->
                         <a href="userLogin.php"><button class="btn btn-primary" id="login" type="submit" >Log in</button></a>
-      
-                        <!--Signup Form -->
                         <a href="userSignup.php"><button class="btn btn-primary" id="signup" type="submit" >Sign Up</button></a>
               </div>';
             }
         ?>
     </header>
 
-    <!-- Main Body -->
+    <!--Main Body-->
     <section id="section1" class="section section1">
         <div class="coursepage-mb">
             <h2><?php echo $courseRow['CourseID']?></h2>
@@ -83,16 +75,15 @@
                                     "<p>".$row['Content']."</p>".
                                     "<p>".$row['PostDate']."</p>".
                                 "</a></div>";
-                            }}
-            
+                            }}   
             ?>
         </section>
 
-    <!-- Footer Section -->
+    <!--Footer Section-->
     <footer>
-        <!-- Contact Information -->
+        <!--Contact Information-->
         <div class="contact-info">
-            <p>Contact Us: wwww.DVA@shareBestie.com | Follow Us: <a href="#">Social Media</a></p>
+            <p>Contact Us: wwww.DVA@shareBestie.com | Follow Us: <a href="#">Facebook</a></p>
         </div>
         <!-- Footer Links -->
         <div class="footer-links">
