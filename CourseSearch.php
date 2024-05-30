f1fef53e03e8f0c0d748bc67bc30f62fe8d7bfbb
cdvincoy <99915734+cdvincoy@users.noreply.github.com>
2024-05-30 11:41:59 +0800

    Final

--- CourseSearch.php
+++ CourseSearch.php
@@ -1,10 +1,10 @@
-<!--Source Code for CourseSearch.php-->
 <?php 
     session_start(); 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--Source Code for CourseSearch.php-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home - shareBestie</title>
